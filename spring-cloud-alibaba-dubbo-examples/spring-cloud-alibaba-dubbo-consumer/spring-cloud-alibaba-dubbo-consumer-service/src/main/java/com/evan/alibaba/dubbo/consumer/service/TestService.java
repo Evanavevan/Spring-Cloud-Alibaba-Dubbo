e08bc423d114f603be11b86300350ace5e54783d
evan <44725772+Evanavevan@users.noreply.github.com>
2021-10-19 17:07:14 +0800

    Add files via upload

---
+++ spring-cloud-alibaba-dubbo-examples/spring-cloud-alibaba-dubbo-consumer/spring-cloud-alibaba-dubbo-consumer-service/src/main/java/com/evan/alibaba/dubbo/consumer/service/TestService.java
@@ -0,0 +1,24 @@
+package com.evan.alibaba.dubbo.consumer.service;
+
+import com.evan.alibaba.dubbo.provider.service.FooService;
+import com.evan.alibaba.dubbo.provider.service.ProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TestService {
+    @Autowired
+    private ProviderService providerService;
+
+    @Autowired
+    private FooService fooService;
+
+    @GetMapping("/test")
+    public String test() {
+        return providerService.hi();
+    }
+
+    @GetMapping("/test/foo")
+    public String testFoo() {
+        return fooService.foo();
+    }
+}
---
+++ spring-cloud-alibaba-dubbo-examples/spring-cloud-alibaba-dubbo-provider/spring-cloud-alibaba-dubbo-provider-service/src/main/java/com/evan/alibaba/dubbo/provider/service/impl/ProviderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.evan.alibaba.dubbo.provider.service.impl;
+
+import com.evan.alibaba.dubbo.provider.service.ProviderService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProviderServiceImpl implements ProviderService {
+    @Override
+    public String hi() {
+        return "Hi Spring Cloud Alibaba Dubbo";
+    }
+}
