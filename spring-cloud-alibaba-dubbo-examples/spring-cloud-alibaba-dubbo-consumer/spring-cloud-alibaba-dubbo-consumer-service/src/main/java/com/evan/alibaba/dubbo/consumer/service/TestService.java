8c0e55ed1fe041d73e76b0b8592497a0ca4f7a7a
evan <44725772+Evanavevan@users.noreply.github.com>
2021-10-19 19:47:16 +0800

    Update TestService.java

--- spring-cloud-alibaba-dubbo-examples/spring-cloud-alibaba-dubbo-consumer/spring-cloud-alibaba-dubbo-consumer-service/src/main/java/com/evan/alibaba/dubbo/consumer/service/TestService.java
+++ spring-cloud-alibaba-dubbo-examples/spring-cloud-alibaba-dubbo-consumer/spring-cloud-alibaba-dubbo-consumer-service/src/main/java/com/evan/alibaba/dubbo/consumer/service/TestService.java
@@ -5,6 +5,7 @@ import com.evan.alibaba.dubbo.provider.service.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 public class TestService {
     @Autowired
     private ProviderService providerService;
